ada62da27b5ad8a06462b9c78249e03440e65e8c
enricoforesta <enrico.foresta97@gmail.com>
2024-02-13 16:37:58 +0100

    feat: seeder type

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -12,12 +13,16 @@ class ProjectSeeder extends Seeder
 {
     public function run(Faker $faker): void
     {
+        Project::truncate();
+
         for ($i = 0; $i < 5; $i++) {
+            $type = Type::inRandomOrder()->first();
             $project = new Project();
             $project->title = $faker->sentence(3);
             $project->description = $faker->text(50);
             $project->slug = Str::of($project->title)->slug('-');
             $project->start_date = $faker->date('Y_m_d');
+            $project->type_id = $type->id;
             $project->save();
         }
     }
